d85653488564b06d3f1f0f3d14549ba3cc9e2db4
André Díaz <andre290800@gmail.com>
2022-03-26 21:32:13 -0600

    chore: BootstrapVue

--- src/main.js
+++ src/main.js
@@ -1,11 +1,16 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
+import { BootstrapVue } from "bootstrap-vue";
 
 import App from "./App.vue";
 import router from "./router";
 
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+
 const app = createApp(App);
 
+app.use(BootstrapVue);
 app.use(createPinia());
 app.use(router);
 
