a6749c08920fc0164542aa8903a5aa85170c4a71
André Díaz <andre290800@gmail.com>
2022-03-28 13:58:07 -0600

    feat: home view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,3 +1,19 @@
+<script setup>
+import { RouterLink } from "vue-router";
+</script>
+
 <template>
-  <div></div>
+  <div
+    class="d-flex flex-column justify-content-center align-items-center h-available"
+  >
+    <h1>¡Bienvenido a Sugar Planet!</h1>
+    <p>Realiza tu pedido a través del siguiente enlace:</p>
+    <RouterLink class="btn btn-primary" to="/order">Ordena ahora</RouterLink>
+  </div>
 </template>
+
+<style>
+.h-available {
+  height: calc(100vh - var(--margin-top-body));
+}
+</style>
