aab04d396002988c16ef6e73401cbc177eebfe20
André Díaz <andre290800@gmail.com>
2022-03-29 19:00:07 -0600

    feat: shop info

---
+++ src/components/ShopInfo.vue
@@ -0,0 +1,10 @@
+<template>
+  <h2>Tienda</h2>
+  <h3>Dirección</h3>
+  <p>Av. Paseo de la Reforma 256, Ciudad de México, CDMX</p>
+  <h3>Teléfono</h3>
+  <p>555-555-1212</p>
+  <h3>Horarios de atención</h3>
+  <p>Lunes a Viernes de 9:00 a 18:00 hrs</p>
+  <p>Sábado de 9:00 a 12:00 hrs</p>
+</template>
