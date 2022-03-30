ffef456be6d07a611096298e6a919339bfcf5912
André Díaz <andre290800@gmail.com>
2022-03-30 00:32:24 -0600

    feat: info view

--- src/views/InfoView.vue
+++ src/views/InfoView.vue
@@ -1,3 +1,11 @@
+<script setup>
+import ShopInfo from "@/components/ShopInfo.vue";
+import FlavoursInfo from "@/components/FlavoursInfo.vue";
+import DecorationsInfo from "@/components/DecorationsInfo.vue";
+</script>
+
 <template>
-  <div></div>
+  <ShopInfo />
+  <FlavoursInfo />
+  <DecorationsInfo />
 </template>
