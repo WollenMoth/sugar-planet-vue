c5d52a5c179061ff503a3f351e1a96ed3090ba95
André Díaz <andre290800@gmail.com>
2022-03-30 20:12:52 -0600

    feat: nested paths

--- src/components/ThemedTableBody.vue
+++ src/components/ThemedTableBody.vue
@@ -1,4 +1,6 @@
 <script setup>
+import _ from "lodash";
+
 defineProps({
   columns: {
     type: Array,
@@ -15,7 +17,7 @@ defineProps({
   <tbody>
     <tr v-for="item in data" :key="item.id">
       <td v-for="column in columns" :key="column.path">
-        {{ item[column.path] }}
+        {{ _.get(item, column.path) }}
       </td>
     </tr>
   </tbody>
