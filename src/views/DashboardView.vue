e8069cbe651a8b2b9df5781fa3f878ca7948ac79
André Díaz <andre290800@gmail.com>
2022-03-30 20:47:58 -0600

    feat: dashboard view

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,3 +1,18 @@
+<script setup>
+import FlavoursTable from "@/components/FlavoursTable.vue";
+import DecorationsTable from "../components/DecorationsTable.vue";
+import OrdersTable from "../components/OrdersTable.vue";
+
+const columns = [
+  { path: "id", label: "#" },
+  { path: "name", label: "Nombre" },
+  { path: "stock", label: "Stock" },
+];
+</script>
+
 <template>
-  <div></div>
+  <h1>Dashboard</h1>
+  <FlavoursTable :columns="columns" />
+  <DecorationsTable :columns="columns" />
+  <OrdersTable />
 </template>
