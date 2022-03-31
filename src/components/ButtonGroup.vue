fb0f080488342802b7b7d3f3b7f88f10c3bb82a5
André Díaz <andre290800@gmail.com>
2022-03-31 03:30:33 -0600

    feat: button group

---
+++ src/components/ButtonGroup.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { useThemeStore } from "@/stores/ThemeStore";
+
+const themeStore = useThemeStore();
+themeStore.fill();
+const theme = themeStore.theme === "dark" ? "light" : "dark";
+</script>
+
+<template>
+  <button type="submit" :class="`btn btn-outline-${theme} me-3`">Enviar</button>
+  <button type="reset" class="btn btn-outline-warning">Reiniciar</button>
+</template>
