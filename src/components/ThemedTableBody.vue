3ca5ccf19df7494ea8559cdd9b7e204957771ab1
André Díaz <andre290800@gmail.com>
2022-03-30 20:46:17 -0600

    feat: correctly render array values

--- src/components/ThemedTableBody.vue
+++ src/components/ThemedTableBody.vue
@@ -11,13 +11,18 @@ defineProps({
     required: true,
   },
 });
+
+const getValue = (item, column) => {
+  const value = _.get(item, column.path);
+  return value instanceof Array ? value.join(", ") : value;
+};
 </script>
 
 <template>
   <tbody>
     <tr v-for="item in data" :key="item.id">
       <td v-for="column in columns" :key="column.path">
-        {{ _.get(item, column.path) }}
+        {{ getValue(item, column) }}
       </td>
     </tr>
   </tbody>
