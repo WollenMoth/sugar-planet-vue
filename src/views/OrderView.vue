822f4c841c4ad76008c2a57cd5ff032abbc394d6
André Díaz <andre290800@gmail.com>
2022-03-31 04:23:10 -0600

    feat: order view

--- src/views/OrderView.vue
+++ src/views/OrderView.vue
@@ -1,3 +1,33 @@
+<script setup>
+import router from "@/router";
+import DescriptionGroup from "@/components/DescriptionGroup.vue";
+import FlavoursGroup from "@/components/FlavoursGroup.vue";
+import DecorationsGroup from "@/components/DecorationsGroup.vue";
+import PersonGroup from "@/components/PersonGroup.vue";
+import ButtonGroup from "@/components/ButtonGroup.vue";
+import { useOrderStore } from "@/stores/OrderStore";
+import { useFormStore } from "@/stores/FormStore";
+
+const orderStore = useOrderStore();
+const formStore = useFormStore();
+
+if (!orderStore.isFilled) orderStore.fill();
+
+formStore.reset();
+
+const submit = () => {
+  orderStore.addOrder(formStore.order);
+  router.push("/");
+};
+</script>
+
 <template>
-  <div></div>
+  <h1>Formulario de pedido de pastel</h1>
+  <form @submit.prevent="submit" class="mb-3">
+    <DescriptionGroup />
+    <FlavoursGroup />
+    <DecorationsGroup />
+    <PersonGroup />
+    <ButtonGroup />
+  </form>
 </template>
