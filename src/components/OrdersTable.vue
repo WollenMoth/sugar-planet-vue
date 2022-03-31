548aec3f23391d50399e4b76260bbf7f251300c6
André Díaz <andre290800@gmail.com>
2022-03-30 20:47:35 -0600

    feat: orders table

---
+++ src/components/OrdersTable.vue
@@ -0,0 +1,21 @@
+<script setup>
+import ThemedTable from "@/components/ThemedTable.vue";
+import { useOrderStore } from "@/stores/OrderStore";
+
+const orderStore = useOrderStore();
+orderStore.fill();
+
+const columns = [
+  { path: "cake.general_description", label: "Descripción General" },
+  { path: "cake.flavours", label: "Sabor" },
+  { path: "cake.decorations", label: "Decoración" },
+  { path: "person.name", label: "Nombre" },
+  { path: "person.phone", label: "Teléfono" },
+  { path: "person.email", label: "Correo Electrónico" },
+];
+</script>
+
+<template>
+  <h2>Pedidos</h2>
+  <ThemedTable :columns="columns" :data="orderStore.orders" />
+</template>
---
+++ src/stores/OrderStore.js
@@ -0,0 +1,16 @@
+import { defineStore } from "pinia";
+
+export const useOrderStore = defineStore("OrderStore", {
+  state: () => ({ orders: [], id: 0 }),
+  actions: {
+    addOrder(order) {
+      this.orders.push({ id: this.id++, ...order });
+    },
+    async fill() {
+      const orders = (await import("@/data/orders.json")).default;
+      for (const order of orders) {
+        this.addOrder(order);
+      }
+    },
+  },
+});
